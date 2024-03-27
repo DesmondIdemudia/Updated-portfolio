ed8b40c0de241b11cc10b6123d69d8991ea653b1
Shedrack Idemudia <shed@Desmonds-MacBook-Air.local>
2024-03-27 12:05:17 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function togglemenu(){
+    const menu = document.querySelector("menu-links");
+    const icon = document.querySelector("hamburger-icon");
+    menu.classList.togglemenu("open");
+    icon.classList.togglemenu("open");
+}
